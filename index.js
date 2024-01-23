b42d4c76a69888da26fa7761eef78bca3b04d329
blackmagic <rikvervoort@gmail.com>
2024-01-23 19:04:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Typer = {
   text: '',
   accessCountimer: null,
   index: 0,
-  speed: 2,
+  speed: 1,
   file: '',
   accessCount: 0,
   deniedCount: 0,
@@ -95,7 +95,7 @@ function replaceUrls(text) {
   }
 }
 
-Typer.speed = 3;
+Typer.speed = 1;
 Typer.file = 'emperor.html';
 Typer.init();
 
